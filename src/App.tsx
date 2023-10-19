702fb471093842e400e9711f8ce4c2f94ca7116c
Nic Woods <nicwoods63@gmail.com>
2023-10-18 23:00:04 -0500

    Add controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,24 @@
 import "./App.css";
 import Spinner from "./Spinner";
 export default function App() {
-  const segments = [
-    "Happy",
-    "Angry",
-    "Sad",
-    "Frustration",
-    // "Emptyness",
-    // "Lol",
-    // "WTF",
-    // "OMG",
-  ];
-  const segmentColors = [
-    "#EE4040",
-    "#F0CF50",
-    "#815CD1",
-    "#3DA5E0",
-    // "#34A24F",
-    // "#F9AA1F",
-    // "#EC3F3F",
-    // "#FF9000",
-  ];
+  const segments = ["Happy", "Angry", "Sad", "Frustration"];
+  const segmentColors = ["#EE4040", "#F0CF50", "#815CD1", "#3DA5E0"];
   const onFinished = (winner: any) => {
     console.log(winner);
   };
   return (
     <div className="App">
-      <div id="wheelCircle">
-        <Spinner
-          segments={segments}
-          size={290}
-          segmentColors={segmentColors}
-          // segColors={segColors}
-          // onFinished={onFinished}
-        />
+      <div className="wrapper">
+        <section className="control-panel">Controls</section>
+        <div id="wheelCircle">
+          <Spinner
+            segments={segments}
+            size={290}
+            segmentColors={segmentColors}
+            // segColors={segColors}
+            // onFinished={onFinished}
+          />
+        </div>
       </div>
     </div>
   );
